40483d81d68428bd26d46d27293a2cf6b5333128
Akshay Neje <akshayneje2823@gmail.com>
2022-07-17 23:02:53 +0530

    Contact App

---
+++ src/Components/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "./Card";
+import contact from "./contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={contact[0].name}
+        img={contact[0].imgURL}
+        tel={contact[0].phone}
+        email={contact[0].email}
+      />
+       <Card
+        name={contact[1].name}
+        img={contact[1].imgURL}
+        tel={contact[1].phone}
+        email={contact[1].email}
+      />
+       <Card
+        name={contact[2].name}
+        img={contact[2].imgURL}
+        tel={contact[2].phone}
+        email={contact[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
