b02ffec5c69be4eb149c5e319111c06f62403fad
Akshay Neje <akshayneje2823@gmail.com>
2022-07-18 19:48:42 +0530

    Condition rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './KeeperApp/Components/App';
+import App from './Conditional Rendering/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
