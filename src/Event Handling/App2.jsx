e5c9d227ccff2b2f3b4f966615117f589a8d740b
Akshay Neje <akshayneje2823@gmail.com>
2022-07-20 18:15:29 +0530

    Done

--- src/Event Handling/App2.jsx
+++ src/Event Handling/App2.jsx
@@ -2,17 +2,23 @@ import React,{useState} from 'react'
 
 function App2() {
     const [name, setName] = useState("");
+    const [getName, setgetName] = useState(" ")
 
     function onChangeHandler(event){
         setName(event.target.value);
-        console.log(event.target.value)
+        // console.log(event.target.value)
+    }
+    function onclickHandler(event){
+        // console.log(setgetName)
+        // console.log(name);
+        setgetName(name);
     }
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
-      <input type="text" placeholder="What's your name?"  onChange={onChangeHandler} />
+      <h1>Hello {getName}</h1>
+      <input  type="text" placeholder="What's your name?"  onChange={onChangeHandler} />
       <button 
-     
+      onClick={onclickHandler}
       >Submit</button>
     </div>
   )
