a4da82dab5761f696e7f2ae21df61093cc1be80c
Akshay Neje <akshayneje2823@gmail.com>
2022-07-20 18:53:04 +0530

    Form Handling

---
+++ src/changing Complex state/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container'>
+        <h1>Hello</h1>
+        <form action="">
+            <input type="text" placeholder='First Name' />
+            <input type="text" placeholder='Last Name' />
+            <button>Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App2 from './Event Handling/App';
-import App2 from './Event Handling/App2';
+import App from '../src/changing Complex state/App'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App2/>
+    <App/>
   </React.StrictMode>
 );
 
