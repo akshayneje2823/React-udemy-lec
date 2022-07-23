3787c3fae146060abc40809d2d09e8e1f1701cf4
Akshay Neje <akshayneje2823@gmail.com>
2022-07-23 20:54:17 +0530

    Done

---
+++ src/KeeperApp/Components/App2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App2() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default App2
--- src/To-Do-List/App.jsx
+++ src/To-Do-List/App.jsx
@@ -1,56 +1,64 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import InpuArea from './InpuArea';
 import To_Dos from './To_Dos';
 
 function App() {
-     
-    const [inputText, setInputText] = useState("");
-    const [inputItems,setInputItems] = useState([])
 
-    function inputHandler(event){
-       let inputValue = event.target.value
-      //  console.log(inputValue)
-       setInputText(inputValue)
-    };
+  const [inputText, setInputText] = useState("");
+  const [inputItems, setInputItems] = useState([])
 
-  function getItemsHandler(){
-    setInputItems((prevItems)=>{
+
+  function inputHandler(event) {
+    let inputValue = event.target.value
+    //  console.log(inputValue)
+    setInputText(inputValue)
+  };
+
+
+
+  function getItemsHandler() {
+    setInputItems((prevItems) => {
       // console.log(prevItems)
-      return [...prevItems ,inputText]
+      return [...prevItems, inputText]
     });
     setInputText("")
   }
-  const deleteItem = (id) =>{
-    setInputItems(prevItems =>{
-      return prevItems.filter((item,index)=>{
+
+  
+
+  const deleteItem = (id) => {
+    setInputItems(prevItems => {
+      return prevItems.filter((item, index) => {
         return index !== id
       })
     })
   }
 
+
+
   return (
     <div className='container'>
-        <div className="heading">
-            <h1>To-Do-List</h1>
-        </div>
-        <div className="form">
-            <InpuArea
-            inputText={inputText}
-            inputHandler={inputHandler}
-            getItemsHandler={getItemsHandler}
-            />
-        </div>
-            <ul>
-                {
-                    inputItems.map((toDoItems,index)=> 
-                       <To_Dos 
-                       key={index}
-                       id={index}
-                       deleteItem={deleteItem} 
-                       toDoItems={toDoItems} />
-                       )
-                }
-            </ul>
+      <div className="heading">
+        <h1>To-Do-List</h1>
+      </div>
+      <div className="form">
+        <InpuArea
+          inputText={inputText}
+          inputHandler={inputHandler}
+          getItemsHandler={getItemsHandler}
+        />
+      </div>
+      <ul>
+        {
+          inputItems.map((toDoItems, index) =>
+            <To_Dos
+              key={index}
+              id={index}
+              deleteItem={deleteItem}
+              toDoItems={toDoItems} />
+          )
+        }
+      </ul>
     </div>
   )
 }
